6139cac1212c0fe43731b767d649d41f05f83d05
Stephanie Ferrier <sferrier96@outlook.com>
2026-02-02 18:26:04 +0000

    WIP: working on addition logic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//storage 
-//addition questions
-string additionQuestions = new[]
+//ADDITION
+//questions
+string[] additionQuestions = new[]
 {
     "35 + 67 = ?",
     "89 + 51 = ?",
@@ -8,8 +9,18 @@ string additionQuestions = new[]
     "23 + 98 = ?",
     "12 + 77 = ?"
 };
+//answers
+string[] additionAnswers = new[]
+{
+    "102",
+    "140",
+    "138",
+    "121",
+    "89"
+};
+
 //subtraction questions
-string subtractionQuestions = new[]
+string[] subtractionQuestions = new[]
 {
     "100 - 13 = ?",
     "56 - 8 = ?",
@@ -18,7 +29,7 @@ string subtractionQuestions = new[]
     "49 - 28 = ?"
 };
 //division questions
-string divisionQuestions = new[]
+string[] divisionQuestions = new[]
 {
     "35 / 7 = ?",
     "89 / 9 = ?",
@@ -27,7 +38,7 @@ string divisionQuestions = new[]
     "77 / 11 = ?"
 };
 //multiplication questions
-string multiplicationQuestions = new[]
+string[] multiplicationQuestions = new[]
 {
     "35 * 80 = ?",
     "12 * 45 = ?",
@@ -36,10 +47,6 @@ string multiplicationQuestions = new[]
     "78 * 2 = ?"
 };
 //surpise questions - this will be 5 randomly selected questions from any of the above.
-
-
-
-
 
 
 Console.WriteLine("Welcome to the MathGame!/nPlease enter your name:");
@@ -61,9 +68,37 @@ switch (userInput){
     case null:
         Console.WriteLine("Please enter an option:");
         break;
+
+        //addition logic
+        //note: remember user tests and validation/error handling
     case "1":
-        Console.WriteLine("Please enter an option:");
+        // think I may need a loop within a loop
+        foreach (string question in additionQuestions)
+        {
+            Console.WriteLine(question.ToString());
+            int counter = 0;
+            string? userAnswer = Console.ReadLine();
+            int number;
+
+            if ( userAnswer == null)
+            {
+                Console.WriteLine("Please enter an answer:");
+            } //this might needing extending ? add Console.ReadLine(); here. 
+
+            //confirm they have entered a number
+            while (int.TryParse(userAnswer, out number ))
+            {
+                if(userAnswer == additionAnswers[counter])
+                {
+                    Console.WriteLine("Correct!");
+                    ++counter;
+
+
+                }
+            }
+        }
         break;
+
     case "2":
         Console.WriteLine("Please enter an option:");
         break;
